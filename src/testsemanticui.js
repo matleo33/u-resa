74fb55382298af290e33722ce0f8d5ea1fec05a7
Nathan MARI <nathan.mari@numericable.fr>
2020-05-11 12:52:36 +0200

    test for semantic ui react

---
+++ src/testsemanticui.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
+
+
+const CardExampleCard = () => (
+  <Card>
+    <Image src='https://react.semantic-ui.comhttps://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Mathieu</Card.Header>
+      <Card.Meta>
+        <span className='date'>Test lib semantic ui react</span>
+      </Card.Meta>
+      <Card.Description>
+        Leothaud la salope
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+        22 Friends non je déconne il a pas d'ami
+      </a>
+    </Card.Content>
+  </Card>
+)
+
+export default CardExampleCard
