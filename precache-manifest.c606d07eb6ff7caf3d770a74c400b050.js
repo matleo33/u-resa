b7e6c0cbcf33c705afa46c76105ae370897a0de2
Nathan MARI <nathan.mari@numericable.fr>
2020-05-11 11:59:51 +0200

    Updates

---
+++ precache-manifest.c606d07eb6ff7caf3d770a74c400b050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09051d481c7f782e9756f7a72bc5b704",
+    "url": "/u-resa/index.html"
+  },
+  {
+    "revision": "39d22313d181bc15301d",
+    "url": "/u-resa/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "9483972d0d8544d3850e",
+    "url": "/u-resa/static/js/2.c72db578.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/u-resa/static/js/2.c72db578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39d22313d181bc15301d",
+    "url": "/u-resa/static/js/main.c19600ec.chunk.js"
+  },
+  {
+    "revision": "45d1744787df0b2cb74f",
+    "url": "/u-resa/static/js/runtime-main.5b6c7351.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/u-resa/static/media/logo.ee7cd8ed.svg"
+  }
+]);
