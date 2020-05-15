ae1af3cf6463f26269c4fc58f58d1471f5c3d1a4
Nathan MARI <nathan.mari@numericable.fr>
2020-05-15 16:20:44 +0200

    Added confirmation (or not) message after the mail is sent

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import '../CSS/profil.css';
 import { Card, Icon, Image, Accordion, Grid } from 'semantic-ui-react'
 import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
+import { Message } from 'semantic-ui-react'
+
 
 export default class Profile extends React.Component {
   state = { activeIndex: 0 }
@@ -10,7 +12,9 @@ export default class Profile extends React.Component {
     this.state = {
       Message: '',
       Object: '',
-      AdresseMail: ''
+      AdresseMail: '',
+      hiddenMessageOK: true,
+      hiddenMessageKO: true
     }
   }
 
@@ -43,16 +47,20 @@ export default class Profile extends React.Component {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        from: this.state.AdresseMail,  
+        from: this.state.AdresseMail,
         subject: this.state.Object,
         message: this.state.Message
       })
-    }).then((response) => response.json())
-      .then((json) => {
-        return json.movies;
-      })
+    }).then((response) => {
+      if (response.status === 200) {
+        console.log("ok");
+        this.setState({ hiddenMessageOK: false })
+
+      }
+    })
       .catch((error) => {
         console.error(error);
+        this.setState({ hiddenMessageKO: false })
       });
   }
 
@@ -60,14 +68,27 @@ export default class Profile extends React.Component {
     const { index } = titleProps
     const { activeIndex } = this.state
     const newIndex = activeIndex === index ? -1 : index
-
     this.setState({ activeIndex: newIndex })
   }
 
   render() {
-    const { activeIndex } = this.state
+    const { activeIndex, hiddenMessageOK, hiddenMessageKO } = this.state
     return (
       <div class="ProfilStyle">
+        <Message positive
+          hidden={hiddenMessageOK}>
+          <Message.Header>Message envoyé</Message.Header>
+          <p>
+            Votre retour nous sera très utile pour améliorer cette application lors de sa prochaine mise à jour.
+    </p>
+        </Message>
+        <Message negative
+          hidden={hiddenMessageKO}>
+          <Message.Header>Message bloqué</Message.Header>
+          <p>
+            Le service est momentanément intérompu. Vous pouvez cependant nous envoyer un mail à uresaprojet@gmail.com.
+    </p>
+        </Message>
         <div class="ProfilImg">
           <Image src='https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-1/p240x240/69733501_10216927294193774_169972699379007488_o.jpg?_nc_cat=102&_nc_sid=dbb9e7&_nc_ohc=wPZc_aqRDk8AX_i0PG5&_nc_ht=scontent-cdg2-1.xx&_nc_tp=6&oh=933ff4e4be69f5f743a5ba94da8254cb&oe=5EDF8E57' size='medium' circular />
         </div>
@@ -88,7 +109,7 @@ export default class Profile extends React.Component {
                   control={Input}
                   label='Email'
                   placeholder='joe@gmail.com'
-                  onChange = {this.onChangeMail.bind(this)}
+                  onChange={this.onChangeMail.bind(this)}
                   value={this.state.AdresseMail}
                 />
                 <Form.Field
@@ -96,7 +117,7 @@ export default class Profile extends React.Component {
                   control={Input}
                   label='Objet'
                   placeholder='Objet du message'
-                  onChange = {this.onChangeObj.bind(this)}
+                  onChange={this.onChangeObj.bind(this)}
                   value={this.state.Object}
                 />
                 <Form.Field
@@ -104,7 +125,7 @@ export default class Profile extends React.Component {
                   control={TextArea}
                   label='Message'
                   placeholder='Message'
-                  onChange = {this.onChangeMsg.bind(this)}
+                  onChange={this.onChangeMsg.bind(this)}
                   value={this.state.Message}
                 />
                 <Form.Field
@@ -131,8 +152,8 @@ export default class Profile extends React.Component {
             </Accordion.Content>
 
             <Accordion.Title active={1}>
-            
-            info 3
+
+              info 3
 
         </Accordion.Title>
 
