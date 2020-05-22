534c4f442caf2f1de1753fd0599cf0a4179ad9c4
Matleo33 <mathieu.leothaud@etu.u-bordeaux.fr>
2020-05-22 12:36:38 +0200

    App add

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Menu from "./Components/Menu"
 import Footer from "./Components/Footer"
 import Routes from "./Components/Routes"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Menu />
-        <Routes />
-        <Footer />
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Menu />
+          <Routes />
+          <Footer />
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
