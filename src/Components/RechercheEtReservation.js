0588f02bfd9cbbfec68ca13f5ab661014e4ab3c1
Nathan MARI <nathan.mari@numericable.fr>
2020-05-25 18:00:55 +0200

    Request sent with constant values in rechercheretreservation

--- src/Components/RechercheEtReservation.js
+++ src/Components/RechercheEtReservation.js
@@ -1,61 +1,64 @@
 import React from 'react';
 import "../CSS/Recherche.css"
-import { Button, Segment } from 'semantic-ui-react'
+import { Button, Table } from 'semantic-ui-react'
 
 
 
 export default class Reservation extends React.Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
             numbers: ["A", "B"],
             Date: "",
-      Horaire: "",
-      Fac: "",
-      Batiment: "",
-      Salle: "",
+            Horaire: "",
+            Fac: "",
+            Batiment: "",
+            Salle: "",
+            response: []
         }
     };
 
-    routeChange = (e) => {
-        e.preventDefault();
-        window.location = './CGU';
-    }
 
-    NumberList(props) {
-        const numbers = props.numbers;
-        const listItems = numbers.map((number) =>
-            <li class="listD">{number}</li>
-        );
-        return (
-            <ul>{listItems}</ul>
-        ); 
-    }
-
-
-      componentDidMount(){
+    componentDidMount() {
         const { data } = this.props.location
         this.setState({
             Date: data[0]["Date"],
-      Horaire: data[0]["Horaire"],
-      Fac: data[0]["Fac"],
-      Batiment: data[0]["Batiment"],
-      Salle: data[0]["Salle"],
-          })
-        }
+            Horaire: data[0]["Horaire"],
+            Fac: data[0]["Fac"],
+            Batiment: data[0]["Batiment"],
+            Salle: data[0]["Salle"]
+        });
+        console.log(data[0]["Date"] + ' ' + data[0]["Horaire"])
+        console.log("ok")
+        fetch("http://localhost:8080/Salles/Disponibilitehoraire", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                horaire: "19/5/2020 14:00",//data[0]["Date"] + ' ' + data[0]["Horaire"],
+                horairefin: "19/5/2020 16:00",//data[0]["Date"] + ' ' + data[0]["Horaire"],
+            })
+        }).then(response => response.json())
+            .then(response => this.setState({ response: response }))
+    }
 
     render() {
-        var numbers = ["{A,B,C}", "{A,B,C}", "{A,B,C}"];
-
         return <div>
             <section class="container-fluid reserv">
                 <div class="TakeReservation">
                     <h2> Propositions de réservation </h2>
                     <hr class="separator"></hr>
                     <div class="reservForm">
-                        <p class="listD">Pellentesque habitant morbi tristique senectus.</p>
-                        <this.NumberList numbers={numbers} />
+                        <ul>
+                            {this.state.response.map((line) =>
+                                <li>
+                                    {line.nomSalle}
+                                </li>
+                            )}
+                        </ul>
                     </div>
                     <div class="ReservationBtn">
                         <Button primary onClick={this.routeChange}>Réserver</Button>
