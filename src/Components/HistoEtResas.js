ac18faf5ade4eb4bbbb227daf711514d8718473f
Matleo33 <mathieu.leothaud@etu.u-bordeaux.fr>
2020-05-25 11:57:23 +0200

    Nathan, teste stp

--- src/Components/HistoEtResas.js
+++ src/Components/HistoEtResas.js
@@ -1,29 +1,88 @@
 import React from 'react';
 import "../CSS/Reservation.css"
-import { Button } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
-
+import { Button, Divider, Table } from 'semantic-ui-react'
 
 export default class Reservation extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
+            visibilityHistoric: "none"
         };
     }
 
+    toggleVisibilityHistoric() {
+        if (this.state.visibilityHistoric == "visible") {
+            this.setState({ visibilityHistoric: "hidden" });
+        } else {
+            this.setState({ visibilityHistoric: "visible" });
+        }
+    }
 
+    componentDidMount() {
+        fetch("http://localhost:8080/Historique")
+            .then(response => response.json())
+            .then(response => this.setState({ historic: response }))
+        fetch("http://localhost:8080/Encours")
+            .then(response => response.json())
+            .then(response => this.setState({ reservations: response }))
+    }
 
     render() {
-
         return <div>
             <section class="container-fluid reserv">
                 <div class="ReservationFirst">
                     <h2> Mes réservations </h2>
-                    <hr class="separator"></hr>
-                    <div id="titi">
-                        <Link to="/u-resa/"><Button secondary>Historique</Button></Link>
-                    </div>
+                    <Divider />
+                    <Table striped>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.Cell>
+                                    Date
+                                </Table.Cell>
+                                <Table.Cell>
+                                    Salle
+                                </Table.Cell>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {this.state.reservations.map((line) =>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        {line.date}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {line.salle}
+                                    </Table.Cell>
+                                </Table.Row>
+                            )}
+                        </Table.Body>
+                    </Table>
+                    <Divider />
+                    <Button onClick={() => this.toggleVisibilityHistoric()}>Voir/Cacher mon historique</Button>
+                    <Table striped style={{ visibility: this.state.visibilityHistoric }}>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.Cell>
+                                    Date
+                                </Table.Cell>
+                                <Table.Cell>
+                                    Salle
+                                </Table.Cell>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {this.state.historic.map((line) =>
+                                <Table.Row>
+                                    <Table.Cell>
+                                        {line.date}
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        {line.salle}
+                                    </Table.Cell>
+                                </Table.Row>
+                            )}
+                        </Table.Body>
+                    </Table>
                 </div>
             </section>
         </div>
