e6cbf6d1259f90d98611b2904e9123b4845d70a4
Nathan MARI <nathan.mari@numericable.fr>
2020-05-26 11:40:32 +0200

    3 requests

--- src/Components/RechercheEtReservation.js
+++ src/Components/RechercheEtReservation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../CSS/Recherche.css"
-import { Button, Table } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 
 
@@ -12,6 +12,7 @@ export default class Reservation extends React.Component {
             numbers: ["A", "B"],
             Date: "",
             Horaire: "",
+            Duree: "",
             Fac: "",
             Batiment: "",
             Salle: "",
@@ -25,24 +26,58 @@ export default class Reservation extends React.Component {
         this.setState({
             Date: data[0]["Date"],
             Horaire: data[0]["Horaire"],
+            Duree: data[0]["Duree"],
             Fac: data[0]["Fac"],
             Batiment: data[0]["Batiment"],
             Salle: data[0]["Salle"]
         });
-        console.log(data[0]["Date"] + ' ' + data[0]["Horaire"])
-        console.log("ok")
-        fetch("http://localhost:8080/Salles/Disponibilitehoraire", {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                horaire: "19/5/2020 14:00",//data[0]["Date"] + ' ' + data[0]["Horaire"],
-                horairefin: "19/5/2020 16:00",//data[0]["Date"] + ' ' + data[0]["Horaire"],
-            })
-        }).then(response => response.json())
-            .then(response => this.setState({ response: response }))
+        if (data[0]["Horaire"] === "Aucune préférence") {
+            fetch("http://localhost:8080/Salles/Disponibilitesalle", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    horaire: data[0]["Date"] + ' ' + data[0]["Horaire"],
+                    idsalle: data[0]["Salle"]
+                })
+            }).then(response => response.json())
+                .then(response => this.setState({ response: response }))
+        }
+        else if (data[0]["Salle"] === "Aucune préférence") {
+            fetch("http://localhost:8080/Salles/Disponibilitehoraire", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    horaire: data[0]["Date"] + ' ' + data[0]["Horaire"],
+                    horairefin: data[0]["Duree"],
+                })
+            }).then(response => response.json())
+                .then(response => this.setState({ response: response }))
+        }
+        else {
+            var str = data[0]["Horaire"].split("H")
+            var heuresansretenue = (Number(str[0]) + Number(Math.trunc((data[0]["Duree"] * 30) / 60))) + 'H' + (Number(str[1]) + Number((data[0]["Duree"] * 30) % 60))
+            var str2 = heuresansretenue.split("H")
+            var heure = data[0]["Date"] + ' ' + (Number(str2[0]) + Number((Math.trunc(Number(str2[1]) / 60)))) + 'H' + (Number(str2[1]) % 60)
+            fetch("http://localhost:8080/Salles/Disponibilite", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    horaire: data[0]["Date"] + ' ' + data[0]["Horaire"],
+                    horairefin: heure,
+                    idsalle: data[0]["Salle"],
+                })
+            }).then(response => response.json())
+                .then(response => this.setState({ response: response }))
+        }
     }
 
     render() {
