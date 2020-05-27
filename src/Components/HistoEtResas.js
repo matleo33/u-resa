216d9917b5f60e3724d48dfc45e79edc3013aa1c
Matleo33 <mathieu.leothaud@etu.u-bordeaux.fr>
2020-05-27 09:31:44 +0200

    Fin HistoResa

--- src/Components/HistoEtResas.js
+++ src/Components/HistoEtResas.js
@@ -2,6 +2,38 @@ import React from 'react';
 import "../CSS/Reservation.css"
 import { Button, Divider, Table } from 'semantic-ui-react'
 
+const TableResa = (props) => {
+    const visibilityMethod = props.visibility || "visible";
+    if (props.content.length === 0) {
+        return <div style={{ "text-align": "center", visibility: visibilityMethod }}>Vous n'avez encore fait aucune réservation</div>
+    } else {
+        return <Table striped collapsing style={{ "margin-left": "auto", "margin-right": "auto", visibility: visibilityMethod }}>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>
+                        Date et heure
+                                </Table.HeaderCell>
+                    <Table.HeaderCell>
+                        Salle
+                                </Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {props.content.map((line) =>
+                    <Table.Row>
+                        <Table.Cell>
+                            {line.horaire}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {line.nomSalle}
+                        </Table.Cell>
+                    </Table.Row>
+                )}
+            </Table.Body>
+        </Table>
+    }
+}
+
 export default class Reservation extends React.Component {
     constructor(props) {
         super(props);
@@ -35,56 +67,12 @@ export default class Reservation extends React.Component {
                 <div class="ReservationFirst">
                     <h2> Mes réservations </h2>
                     <Divider />
-                    <Table striped>
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell>
-                                    Date
-                                </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    Salle
-                                </Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {this.state.reservations.map((line) =>
-                                <Table.Row>
-                                    <Table.Cell>
-                                        {line.horaire}
-                                    </Table.Cell>
-                                    <Table.Cell>
-                                        {line.nomSalle}
-                                    </Table.Cell>
-                                </Table.Row>
-                            )}
-                        </Table.Body>
-                    </Table>
+                    <TableResa content={this.state.reservations} />
                     <Divider />
-                    <Button onClick={() => this.toggleVisibilityHistoric()}>Voir/Cacher mon historique</Button>
-                    <Table striped style={{ visibility: this.state.visibilityHistoric }}>
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell>
-                                    Date
-                                </Table.HeaderCell>
-                                <Table.HeaderCell>
-                                    Salle
-                                </Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            {this.state.historic.map((line) =>
-                                <Table.Row>
-                                    <Table.Cell>
-                                        {line.horaire}
-                                    </Table.Cell>
-                                    <Table.Cell>
-                                        {line.nomSalle}
-                                    </Table.Cell>
-                                </Table.Row>
-                            )}
-                        </Table.Body>
-                    </Table>
+                    <div style={{ "textAlign": "center", marginBottom: "20px" }}>
+                        <Button onClick={() => this.toggleVisibilityHistoric()}>Voir/Cacher mon historique</Button>
+                    </div>
+                    <TableResa content={this.state.reservations} visibility={this.state.visibilityHistoric} />
                 </div>
             </section>
         </div>
